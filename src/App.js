88973b3ac5ff99a1e0ded916d89399efc5b31335
Giang Nguyen <giang.nguyen.huong@hotmail.com>
2022-08-18 20:41:59 +1000

    spike condition

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Spike github actions
         </a>
       </header>
     </div>
